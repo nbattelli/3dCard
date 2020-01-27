92d8c248649e0c691f5d942eb0b9f94abfb18f3f
Nicolas Battelli <nicolas.battelli@rappi.com>
2020-01-27 15:41:01 -0500

    Initial Commit

---
+++ RappiCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RappiCard
+//
+//  Created by Nicolas Battelli on 27/01/2020.
+//  Copyright © 2020 Nicolas Battelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
